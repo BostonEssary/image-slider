001ab1e7aeb3c6e3a9301c6e0a4e8eee26ed476b
Boston <BostonCEssary@gmail.com>
2023-10-02 16:48:51 -0700

    add navigation circles

--- src/index.js
+++ src/index.js
@@ -7,26 +7,38 @@ const leftButton = document.querySelector(".left-button")
 const images = document.querySelectorAll(".image")
 const startingImage = document.getElementById("starting-image")
 const navCirclesContainer = document.querySelector(".nav-circles-container")
+const navCircles = []
 let counter = 0
 
+images.forEach((image) => {
+    
+        const navCircle = document.createElement("div")
+        navCircle.classList.add("nav-circle")
+        navCirclesContainer.append(navCircle)
+        navCircles.push(navCircle)
+})
 
+navCircles[0].classList.add("active")
 
 function handleTimeout(){
     if(counter >= images.length - 1){
         images[counter].classList.add("hidden")
+        navCircles[counter].classList.remove("active")
         counter = 0
         images[counter].classList.remove("hidden")
-        
+        navCircles[counter].classList.add("active")
     }
     else{
         images[counter].classList.add("hidden")
-    counter++
-    images[counter].classList.remove("hidden")
+        navCircles[counter].classList.remove("active")
+        counter++
+        images[counter].classList.remove("hidden")
+        navCircles[counter].classList.add("active")
     }
     
 }
 
-setInterval(handleTimeout, 5000)
+const timer = setInterval(handleTimeout, 5000)
 
 rightButton.addEventListener("click", () => {
     if(counter >= images.length - 1){
@@ -36,16 +48,23 @@ rightButton.addEventListener("click", () => {
                 image.classList.add("hidden")
             }
         })
+        navCircles.forEach((circle) => {
+            circle.classList.remove("active")
+        })
         images[0].classList.remove("hidden")
+        navCircles[0].classList.add("active")
     }
     else{
         images[counter].classList.add("hidden")
+        navCircles[counter].classList.remove("active")
         counter++
         images[counter].classList.remove("hidden")
+        navCircles[counter].classList.add("active")
     }
     
     
-    console.log(counter)
+    clearInterval(timer)
+    setInterval(handleTimeout, 5000)
 })
 
 leftButton.addEventListener("click", () => {
@@ -56,15 +75,26 @@ leftButton.addEventListener("click", () => {
                 image.classList.add("hidden")
             }
         })
+        navCircles.forEach((circle) => {
+            circle.classList.remove("active")
+        })
         images[0].classList.add("hidden")
+        navCircles[0].classList.remove("active")
+
         images[counter].classList.remove("hidden")
+        navCircles[counter].classList.add("active")
+
     }
     else{
         images[counter].classList.add("hidden")
+        navCircles[counter].classList.remove("active")
         counter--
         images[counter].classList.remove("hidden")
+        navCircles[counter].classList.add("active")
+
     }
     
     
-    console.log(counter)
+    clearInterval(timer)
+    setInterval(handleTimeout, 5000)
 })
